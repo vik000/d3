7c97281309c8180fa472ac63967997e2de28658c
Victor Manuel Garcia Sanabria <vik000@gmail.com>
2021-12-15 23:55:54 +0100

    maps

---
+++ 14/map-chloropeth/main.js
@@ -0,0 +1,72 @@
+
+const width = 800
+const height = 400
+const margin = {
+    top: 50, 
+    right: 50, 
+    left: 50, 
+    bottom: 50
+}
+
+const svg = d3.select('#map').append('svg').attr("width", width).attr("height", height)
+const elementGroup = svg.append("g").attr("id", "elementGroup")
+let back = elementGroup.append("rect").attr("id", "back").attr("width", width).attr("height", height).attr("fill", "transparent")
+let tooltipGroup = svg.append("g").attr("id", "tooltipGroup")
+tooltipGroup.append("rect").attr("width", 200).attr("height", 15).attr("fill", "white")
+let tooltip = tooltipGroup.append('text').attr("id", "tooltip")
+
+back.on("mouseover", function() {
+    tooltipGroup.classed("hidden", true)
+})
+
+let colour = d3.scaleSequential().interpolator(d3.interpolateBlues); 
+
+const projection = d3.geoMercator().translate([width / 2, height / 2]).scale(100)
+let path = d3.geoPath().projection(projection)
+
+let promises = [d3.json("world.topojson"), d3.csv('data.csv')]
+
+Promise.all(promises).then(dataSources => {
+    data = dataSources[0]
+    data2 = dataSources[1]
+
+    colour.domain(d3.extent(data2.map(d => +d["Gross public debt"])))
+    countriesAlone = data2.map(d => d.Country)
+    let countries = topojson.feature(data, data.objects.countries).features
+
+    // combine both arrays: 
+    countries.map(d => {
+        index = countriesAlone.findIndex(name => name == d.properties.name)
+        if(index != -1) {
+            val = +data2[index]["Gross public debt"]
+            d.properties["debt"] = val
+        }
+    })
+    let map = elementGroup.selectAll("path.country").data(countries)
+    map.call(drawMap)
+})
+
+function drawMap(selection) {
+     selection.enter()
+        .append('path')
+            .attr('class', 'country')
+            .attr("d", path)
+            .attr("fill", d => d.properties.debt ? colour(d.properties.debt) : "lightgray")
+            .on("mousemove", function(d) {
+                console.log(this)
+                mouse = d3.mouse(this)
+                name = d.properties.name
+                debt = d.properties.debt != undefined ? d.properties.debt : "unknown"
+   
+                tooltipGroup.classed("hidden", false)            
+                tooltipGroup
+                    .attr("transform", `translate(${mouse[0] + 20}, ${mouse[1] + 20})`)
+                tooltip
+                    .attr("fill", "black")
+                    .attr("y", 12)
+                    .text(`${name}: ${debt}%`)
+                tooltipGroup.select('rect').attr("width", tooltip.node().getBoundingClientRect().width)
+                // console.log(tooltip)
+
+            })
+}
--- 14/map0/main.js
+++ 14/map0/main.js
@@ -1,30 +1,32 @@
-// constants:
-const width = 500
-const height = 500
+const width = 800
+const height = 400
+const margin = {
+    top: 50, 
+    right: 50, 
+    left: 50, 
+    bottom: 50
+}
 
+const svg = d3.select('#map').append('svg').attr("width", width).attr("height", height)
+const elementGroup = svg.append("g").attr("id", "elementGroup")
+const projection = d3.geoMercator().translate([width / 2, height / 2]).scale(100)
+let path = d3.geoPath().projection(projection)
 
-// The svg
-let svg = d3.select('#chart').append('svg').attr("width", width).attr("height", height)
+d3.json("world.topojson").then(data => {
+    let countries = topojson.feature(data, data.objects.countries)
+    console.log(countries)
 
-// Map and projection
-// var projection = d3.geoNaturalEarth1()
-var projection = d3.geoMercator()
-    // .scale(width / 1.3 / Math.PI)
-    .scale(70)
-    // .translate([width / 2, height / 2])
-    .translate([width / 2.5, height / 2.5])
-
-// Load external data and boot
-d3.json("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world.geojson").then(data => {
-    // console.log(data.features);
-    // Draw the map
-    svg.append("g")
-        .selectAll("path")
-        .data(data.features)
-        .enter().append("path")
-            .attr("fill", "#69b3a2")
-            .attr("d", d3.geoPath()
-                .projection(projection)
-            )
-            .style("stroke", "#fff")
+    let map = elementGroup.selectAll("path.country").data(countries.features)
+    map.call(drawMap)
 })
+
+function drawMap(selection) {
+     selection.enter()
+        .append('path')
+            .attr('class', 'country')
+            .attr("d", path)
+            .on("mouseover", function() {
+                d3.selectAll("path.country").classed("hover", false)
+                d3.select(this).classed("hover", true)
+            })
+}
